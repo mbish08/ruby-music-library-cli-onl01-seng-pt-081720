08452037e26664f28ae587cf707aeff93ab2d6e5
Mikel Bishop <mdrew8@gmail.com>
2020-09-28 01:12:34 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+class Song 
+  
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def save
+    @@all << self 
+  end 
+  
+  def self.destroy_all 
+    @@all.clear
+  end 
+  
+  def self.create(song)
+    song = self.new(song)
+    song.save
+    song  
+  end 
+  
+  
+  
+end 
