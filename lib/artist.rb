8d9cc06cff9e3e51ac2a0f9127f9ded645bc3e46
Mikel Bishop <mdrew8@gmail.com>
2020-09-29 01:39:32 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+class Artist 
+  
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.destroy_all 
+    @@all.clear 
+  end 
+  
+  def save
+    @@all << self 
+  end 
+  
+  def self.create(artist)
+    artist = self.new(artist)
+    artist.save
+    artist 
+  end 
+  
+end 
