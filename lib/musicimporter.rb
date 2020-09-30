e904531490f44689e1d344b20554d1275cf77fd2
Mikel Bishop <mdrew8@gmail.com>
2020-09-30 00:16:51 +0000

    Done.

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -1,5 +1,16 @@
+require 'pry'
+
 class MusicImporter
   
+  attr_accessor :path, :files 
+  
+  def initialize(path)
+    @path = path 
+  end 
+  
+  def files 
+    Dir.entries(@path).select { |entry| entry.include?(".mp3") }
+  end 
   
   
   
